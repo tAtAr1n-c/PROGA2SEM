1709901baea6c11e5b848fdddc9954a55d76316b
Sabirov Mirsait <cabmupcaut@gmail.com>
2026-03-09 10:31:00 +0300

    Tests

---
+++ Spiski/MyArrayListTest.java
@@ -0,0 +1,36 @@
+package Basic;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class MyArrayListTest {
+    private MyList<String> list;
+
+    @BeforeEach
+    void setUp(){
+        list = new MyLinkedList<>();
+    }
+
+    @Test
+    void testAddandSize(){
+        list.add("Test1");
+        list.add("Test2");
+        list.add("Test3");
+        Assertions.assertEquals(true, list.contains("Test3"));
+        Assertions.assertEquals(3, list.size());
+        Assertions.assertEquals(false, list.isEmpty());
+    }
+
+    @Test
+    void testget(){
+        list.add("Test1");
+        list.add("Test2");
+        list.add("Test3");
+        list.add("Test4");
+        list.add("Test5");
+        Assertions.assertEquals("Test3", list.remove(2));
+        Assertions.assertEquals(3, list.indexOf("Test5"));
+        Assertions.assertEquals(true, list.remove("Test5"));
+    }
+}
